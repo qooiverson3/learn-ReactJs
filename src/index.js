1064d7cecb710f7267fb6cec6a4c326a06eceec1
賴威勝 <sheng@laiweishengdeMacBook-Air.local>
2021-08-14 15:53:54 +0800

    test/import-components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import '@fontsource/roboto';
+// import './index.css';
+// import Welcome from './material-test/welcome';
+// import DataTable from './material-test/dataTable';
+// import CircularStatic from './material-test/process';
+// import XGridDemo from './material-test/dynamicData';
+//import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+import SignInSide from './login/index';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <Welcome />
+    <DataTable />
+    <CircularStatic />
+    <XGridDemo /> */}
+    <SignInSide />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +26,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
---
+++ src/material-test/welcome.js
@@ -0,0 +1,7 @@
+import { Button } from '@material-ui/core';
+
+function Welcome() {
+    return <Button variant="contained" color="primary">Hello World</Button>;
+}
+
+export default Welcome;
